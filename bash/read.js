8bf17d73793e505abd2ec29307750d24454a3bc8
bellcliff <bell.cliff@gmail.com>
2016-02-05 11:46:50 -0500

    refactor

--- bash/read.js
+++ bash/read.js
@@ -12,43 +12,46 @@ var readdir = function(dirname) {
             // TODO, handle directory
             var filename = dirname + "/" + file;
             if (!filename.endsWith('.txt')) return;
-            fs.readFile(filename, 'utf8', function(err, cnt) {
-                //TODO fix later
-                if (err) {
-                    throw err
-                }
-                console.log(filename, cnt.length);
-                var lines = cnt.split(/\r\n/);
-                console.log(lines.length);
-                var interviews = [];
-                var interview;
-                lines.forEach(function(line){
-                    /**
-                     * handle all lines in one method,
-                     * which means we need keep a status
-                     * the status means the line is at 
-                     *   head, interview info or interview questions
-                     */
-                    if (line.startsWith('--') || line.startsWith('==')) {
-                        if (interview !== undefined) {
-                            interviews.push(interview);
-                        }
-                        interview = {};
-                    } else if (line.startsWith('Client')) {
-                        interview.Client = line.split(':')[1];
-                    } else if (line.startsWith('Type')) {
-                        interview.Type = line.split(':')[1];
-                    } else if (line.startsWith('Candidate')) {
-                        interview.Candidate = line.split(':')[1];
-                    } else if (line.startsWith('Date')) {
-                        interview.Date = line.split(':')[1];
-                    }
-                });
-                if (interview !== undefined) interviews.push(interview);
-                console.log('count of interview', interviews.length, interviews[0]);
-            })
+            fs.readFile(filename, 'utf8', function(err, cnt) {})
         })
     })
+}
+
+var parseFile = function() {
+    //TODO fix later
+    if (err) {
+        throw err
+    }
+    console.log(filename, cnt.length);
+    var lines = cnt.split(/\r\n/);
+    console.log(lines.length);
+    var interviews = [];
+    var interview;
+    lines.forEach(function(line) {
+        /**
+         * handle all lines in one method,
+         * which means we need keep a status
+         * the status means the line is at
+         *   head, interview info or interview questions
+         */
+        if (line.startsWith('--') || line.startsWith('==')) {
+            if (interview !== undefined) {
+                interviews.push(interview);
+            }
+            interview = {};
+        } else if (line.startsWith('Client')) {
+            interview.Client = line.split(':')[1];
+        } else if (line.startsWith('Type')) {
+            interview.Type = line.split(':')[1];
+        } else if (line.startsWith('Candidate')) {
+            interview.Candidate = line.split(':')[1];
+        } else if (line.startsWith('Date')) {
+            interview.Date = line.split(':')[1];
+        }
+    });
+    if (interview !== undefined) interviews.push(interview);
+    console.log('count of interview', interviews.length, interviews[0]);
+
 }
 
 readdir(__dirname);
